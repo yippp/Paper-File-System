bd5c7e68028c07cefad3ddf3ffdd08ce16e360fe
punemajisi <115010124@link.cuhk.edu.cn>
2017-12-21 15:32:08 +0800

    error no path
    for test fucking error no path

---
+++ processall.cpp
@@ -0,0 +1,21 @@
+#include "processall.h"
+#include <string>
+#include <vector>
+#include "paper.h"
+#include "txt.h"
+#include "findinfo.h"
+#include "mainwindow.h"
+#include <QApplication>
+using namespace std;
+
+// put all the pdf file directely into the build file
+vector<paper>* process_all() {
+    // check all pdf and find info
+    vector<string> txtList = traversePDF();
+    vector<vector<string>>* data = readStrings(txtList);
+    data = clean(data);
+    vector<paper> *papersList;
+    papersList = findInfo(data);
+    return papersList;
+}
+
---
+++ processall.h
@@ -0,0 +1,13 @@
+#ifndef PROCESSALL_H
+#define PROCESSALL_H
+
+#include <string>
+#include <vector>
+
+#include "paper.h"
+using namespace std;
+
+// put all the pdf file directely into the build file
+vector<paper>* process_all();
+
+#endif // PROCESSALL_H
