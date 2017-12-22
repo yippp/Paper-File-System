5ac932a6dacc28d33447ff60883b37d0384192ea
yippp <115010269@link.cuhk.edu.cn>
2017-12-22 13:17:26 +0800

    change data path
    change data path

--- processall.cpp
+++ processall.cpp
@@ -11,9 +11,6 @@ using namespace std;
 
 vector<paper>* process_all(vector<paper>* papersList, vector<string> txtList, vector<string> &newTxtList) {
     // check all pdf and find info
-    // the below 2 line is used to read from text
-//    vector<paper> *papersList = new vector<paper>;
-//    vector<string> txtList = readFromFile(papersList);
     newTxtList.insert(newTxtList.end(), txtList.begin(), txtList.end());
     vector<paper>* tempPaperList = new vector<paper>;
     vector<string> tempTxtList;
@@ -31,34 +28,27 @@ vector<paper>* process_all(vector<paper>* papersList, vector<string> txtList, ve
     }
     saveToFile(papersList, newTxtList);
     return papersList;
-
-//    vector<string> txtList = traversePDF();
-//    vector<vector<string>>* data = readStrings(txtList);
-//    vector<paper> *papersList;
-//    papersList = findInfo(data);
-//    saveToFile(papersList, txtList);
-//    return papersList;
 }
 
 void saveToFile(vector<paper>* papersList, vector<string> txtList) {
     ofstream file;
-    file.open("../../../file.txt");
+    file.open("../../../data/file.txt");
     ofstream title;
-    title.open("../../../title.txt");
+    title.open("../../../data/title.txt");
     ofstream authors;
-    authors.open("../../../authors.txt");
+    authors.open("../../../data/authors.txt");
     ofstream abstract;
-    abstract.open("../../../abstract.txt");
+    abstract.open("../../../data/abstract.txt");
     ofstream tag;
-    tag.open("../../../tag.txt");
+    tag.open("../../../data/tag.txt");
     ofstream year;
-    year.open("../../../year.txt");
+    year.open("../../../data/year.txt");
     ofstream keywords;
-    keywords.open("../../../keywords.txt");
+    keywords.open("../../../data/keywords.txt");
     ofstream conference;
-    conference.open("../../../conferences.txt");
+    conference.open("../../../data/conferences.txt");
     ofstream DOI;
-    DOI.open("../../../DOI.txt");
+    DOI.open("../../../data/DOI.txt");
 
     for (int i = 0; i < (int)txtList.size(); i++) {
         file << txtList.at(i) << endl;
@@ -81,36 +71,29 @@ void saveToFile(vector<paper>* papersList, vector<string> txtList) {
     keywords.close();
     conference.close();
     DOI.close();
-    //    int i = 0;
-    //    for (string txt : txtList) {
-    //        fstream binary_file("../../../"+ txt + ".dat",ios::out|ios::binary|ios::app);
-    //        binary_file.write(reinterpret_cast<char *>(papersList->at(i)), sizeof(papersList->at(i)));
-    //        binary_file.close();
-    //        i++;
-    //    }
 }
 
 vector<string> readFromFile(vector<paper> *papersList) {
     ifstream file;
-    file.open("../../../file.txt");
+    file.open("../../../data/file.txt");
     vector<string> txtList;
     if(file) {
         ifstream title;
-        title.open("../../../title.txt");
+        title.open("../../../data/title.txt");
         ifstream authors;
-        authors.open("../../../authors.txt");
+        authors.open("../../../data/authors.txt");
         ifstream abstract;
-        abstract.open("../../../abstract.txt");
+        abstract.open("../../../data/abstract.txt");
         ifstream tag;
-        tag.open("../../../tag.txt");
+        tag.open("../../../data/tag.txt");
         ifstream year;
-        year.open("../../../year.txt");
+        year.open("../../../data/year.txt");
         ifstream keywords;
-        keywords.open("../../../keywords.txt");
+        keywords.open("../../../data/keywords.txt");
         ifstream conference;
-        conference.open("../../../conferences.txt");
+        conference.open("../../../data/conferences.txt");
         ifstream DOI;
-        DOI.open("../../../DOI.txt");
+        DOI.open("../../../data/DOI.txt");
 
         string fileLine;
         string titleLine;
@@ -159,10 +142,5 @@ vector<string> readFromFile(vector<paper> *papersList) {
     } else {
         perror("can not find saved data.");
     }
-//    fstream binary_file("../../../paperList.dat",ios::binary|ios::in|ios::app);
-//    binary_file.seekg (0, ios::end);
-//    int size = binary_file.tellg();
-//    binary_file.read(reinterpret_cast<char *>(papersList),size);
-//    binary_file.close();
     return txtList;
 }
